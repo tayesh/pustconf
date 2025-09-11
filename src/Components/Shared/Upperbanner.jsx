a49dd8d8d5823eeec5351b58b501a3e45e8ee670
unknown <tayesh>
2025-09-11 20:55:20 +0600

    changed

--- src/Components/Shared/Upperbanner.jsx
+++ src/Components/Shared/Upperbanner.jsx
@@ -2,7 +2,7 @@ const Upperbanner = () => {
     return (
         <div className="mx-20 my-3 mb-6 flex justify-between">
             <div className="flex items-center gap-3">
-                <img src="https://i.ibb.co/cKCC02YG/pabna-university-of-science-and-technology-275200.png" alt="University Logo" className="h-12 md:h-20" />
+                {/* <img src="https://i.ibb.co/cKCC02YG/pabna-university-of-science-and-technology-275200.png" alt="University Logo" className="h-12 md:h-20" /> */}
                 <div>
                     <p className="text-lg">ICERFEST-2026</p>
                     <p className="text-md uppercase">INTERNATIONAL CONFERENCE On <br />

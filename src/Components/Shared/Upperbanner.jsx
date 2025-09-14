c0e17547521d44838658729fcb7abe1e011564dd
unknown <tayesh>
2025-09-15 00:17:11 +0600

    logo change

--- src/Components/Shared/Upperbanner.jsx
+++ src/Components/Shared/Upperbanner.jsx
@@ -2,11 +2,11 @@ const Upperbanner = () => {
     return (
         <div className="lg:mx-20 my-3 lg:py-0 py-5 mb-6 flex lg:flex-row flex-col justify-between gap-y-5">
             {/* Left side: Title and image */}
-            <div className="flex lg:flex-row flex-col lg:items-end items-center lg:text-start text-center gap-4">
+            <div className="flex lg:flex-row flex-col lg:items-center items-center lg:text-start text-center gap-4">
                 <img
-                    src="https://i.ibb.co/PZW5zfYZ/Untitled-2h.png"
+                    src="https://i.ibb.co.com/B5S0j14n/Whats-App-Image-2025-09-14-at-23-33-58-e20612ec.jpg"
                     alt="University Logo"
-                    className="lg:w-40 w-64"
+                    className="lg:w-32 w-64"
                 />
                 <div className="flex flex-col justify-end">
                     <p className="lg:text-lg lg:block hidden text-2xl">ICERFEST-2026</p>
@@ -18,7 +18,7 @@ const Upperbanner = () => {
             </div>
 
             {/* Right side: Partner logos */}
-            <div className="flex items-end lg:mt-0 mt-5 justify-center pb-0 lg:justify-end gap-6">
+            <div className="flex items-center lg:mt-0 mt-5 justify-center pb-0 lg:justify-end gap-6">
                 <img
                     src="https://i.ibb.co/cKCC02YG/pabna-university-of-science-and-technology-275200.png"
                     alt="University Logo"
